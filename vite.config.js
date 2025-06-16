4af9f89e83176b3b8620c5b84e5e1785df523890
pranaydo <pranaydodake7@gmail.com>
2025-06-16 12:44:31 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()]
+// })
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()]
-})
+  plugins: [react()],
+  base: "/smilemakeover/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        thankyou: resolve(__dirname, "thankyou.html"),
+      },
+    },
+  },
+});
