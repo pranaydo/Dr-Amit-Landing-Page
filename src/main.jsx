4ad4e8b1208e00e1299a1ff62da0b1e041b7ab2d
pranaydo <pranaydodake7@gmail.com>
2025-06-18 16:06:20 +0530

    ASDF

--- src/main.jsx
+++ src/main.jsx
@@ -21,11 +21,11 @@
 //     </BrowserRouter>
 //   </React.StrictMode>
 // );
-import React from "react";
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Thankyou from "./components/Thankyou";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
